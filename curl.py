994d88124d8799a8e3d9c304040b79993cdd3b13
mashuangwe <mashuangwe@163.com>
2018-09-04 15:21:32 +0800

    add seg result

--- curl.py
+++ curl.py
@@ -1,13 +1,11 @@
+# -*- coding: utf-8 -*-
+
 '''
 分词测试
 '''
 
-# -*- coding: utf-8 -*-
-
 import requests
 import time
-import threading
-
 
 headers = {
     'Content-type': 'application/json'
@@ -16,15 +14,36 @@ headers = {
 data = {
     'segOrPos': 'seg',
     'useDict': 'no',
-    'sentences': ['今天天气真好，心情也很好！']
+    'sentences': []
 }
 
 def curl():
     response = requests.post(url='http://172.16.13.117:8091/segPos', headers=headers, json=data)
-    return response.text
+    return response.json()
+
+start = time.time()
+response = []
+with open('query.txt', 'r', encoding='utf-8') as f:
+    line = f.readline()
+    # i = 0
+    while line:
+        # response.append(curl())
+        # if i == 10:
+        #     response += curl()
+        #     data['sentences'] = []
+        #     i = 0
+        data['sentences'].append(line)
+        line = f.readline()
+        # i += 1
 
 response = curl()
+span = time.time() - start
+print(span)
 print(response)
+with open('segResult.txt', 'w', encoding='utf-8') as f:
+    for r in response:
+        f.write(r + '\n')
+
 
 
 # if __name__ == '__main__':
